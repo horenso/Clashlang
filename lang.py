b9339f64c555c4eec4896654066005e57eecfaad
horenso <jannis.adamek@gmail.com>
2022-01-06 01:34:26 +0100

    First commit: Simple Tokenizer

---
+++ lang.py
@@ -0,0 +1,87 @@
+from enum import Enum
+from re import match
+from typing import Mapping
+from sys import stderr
+from textwrap import shorten
+from typing import Type
+
+
+class TokenType(Enum):
+    WHITESPACE = 0
+    NUMBER = 1
+    OPERATOR = 2
+    PARENTHESIS = 3
+    IDENTIFIER = 4
+
+
+RegexMap: Mapping[str, TokenType] = {
+    '[\s\n\t\r]': TokenType.WHITESPACE,
+    '\d+(\.\d+)?': TokenType.NUMBER,
+    '[\+\-\*/]': TokenType.OPERATOR,
+    '[()\[\]]': TokenType.PARENTHESIS,
+    '[a-zA-Z]([a-zA-Z]|\d)*': TokenType.IDENTIFIER
+}
+
+
+class Token:
+    def __init__(self, token_type: TokenType, value: str):
+        self.token_type = token_type
+        self.value = value
+
+    def __repr__(self) -> str:
+        return f"<{self.token_type.name} '{self.value}'>"
+
+
+class Tokenizer:
+    def __init__(self, string: str):
+        self.string = string
+
+    def _match_next_token(self):
+        for pattern, token_type in RegexMap.items():
+            potential_match = match(pattern, self.string)
+            if potential_match:
+                s = potential_match.group(0)
+                self.string = self.string[len(s):]
+                return Token(token_type, s)
+        return None
+
+    def parse(self):
+        while len(self.string) > 0:
+            token = self._match_next_token()
+            if token == None:
+                print(shorten(self.string, width=10,
+                      placeholder='...'), file=stderr)
+                print('^ Invalid token', file=stderr)
+                return
+            if token.token_type == TokenType.WHITESPACE:
+                continue
+            yield token
+
+
+def main():
+    for t in Tokenizer(input('Expression: ')).parse():
+        print(t)
+
+
+if __name__ == '__main__':
+    main()
+
+
+# Grammar:
+# expr   -> factor
+# factor -> (factor)
+# factor -> term * term
+# factor -> term
+# term   -> num + num
+# term   -> num
+
+# 3 + 1232 * ((21 + 21) * 2)
+
+#      (+)  expr -> factor -> term
+#     /  \
+#    3   (*)
+#         |
+#        (*)
+#    /  \   \
+#   21  21
+#
